34731f831e073413b28ab728c78e567b7b97f625
hanjinhui638 <jh312jh@naver.com>
2019-11-27 17:53:11 +0900

    1170553

--- src/main/java/com/mid/pro/model/MemberVO.java
+++ src/main/java/com/mid/pro/model/MemberVO.java
@@ -1,5 +1,111 @@
 package com.mid.pro.model;
 
+import java.sql.Date;
+
 public class MemberVO {
 
+	private int member_num;
+	private String id;
+	private String pw;
+	private String email;
+	private String name;
+	private String tel;
+	private String post1;
+	private String addr1;
+	private String addr2;
+	private Date date;
+	private int writecount;
+	private String kind;
+	private int reg_number;
+	private int grade;
+	
+	
+	public int getMember_num() {
+		return member_num;
+	}
+	public void setMember_num(int member_num) {
+		this.member_num = member_num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getPost1() {
+		return post1;
+	}
+	public void setPost1(String post1) {
+		this.post1 = post1;
+	}
+	public String getAddr1() {
+		return addr1;
+	}
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+	public String getAddr2() {
+		return addr2;
+	}
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getWritecount() {
+		return writecount;
+	}
+	public void setWritecount(int writecount) {
+		this.writecount = writecount;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public int getReg_number() {
+		return reg_number;
+	}
+	public void setReg_number(int reg_number) {
+		this.reg_number = reg_number;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
 }
+
+
+
