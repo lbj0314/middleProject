0c720cf55e5b34e4ea40cbb31dd79f0d43741f2f
hanjinhui638 <jh312jh@naver.com>
2019-12-09 14:29:56 +0900

    12090229

---
+++ src/main/java/com/mid/pro/model/ReservationVO.java
@@ -0,0 +1,66 @@
+package com.mid.pro.model;
+
+public class ReservationVO {
+
+	private int rev_num;
+	private int rest_num;
+	private int member_num;
+	private int rev_people;
+	private String rev_date;
+	private int rev_state;
+	private int menu_num;
+	private String rev_time;
+	
+	
+	public int getRev_num() {
+		return rev_num;
+	}
+	public void setRev_num(int rev_num) {
+		this.rev_num = rev_num;
+	}
+	public int getRest_num() {
+		return rest_num;
+	}
+	public void setRest_num(int rest_num) {
+		this.rest_num = rest_num;
+	}
+	public int getMember_num() {
+		return member_num;
+	}
+	public void setMember_num(int member_num) {
+		this.member_num = member_num;
+	}
+	public int getRev_people() {
+		return rev_people;
+	}
+	public void setRev_people(int rev_people) {
+		this.rev_people = rev_people;
+	}
+	public String getRev_date() {
+		return rev_date;
+	}
+	public void setRev_date(String rev_date) {
+		this.rev_date = rev_date;
+	}
+	public int getRev_state() {
+		return rev_state;
+	}
+	public void setRev_state(int rev_state) {
+		this.rev_state = rev_state;
+	}
+	public int getMenu_num() {
+		return menu_num;
+	}
+	public void setMenu_num(int menu_num) {
+		this.menu_num = menu_num;
+	}
+	public String getRev_time() {
+		return rev_time;
+	}
+	public void setRev_time(String rev_time) {
+		this.rev_time = rev_time;
+	}
+	
+	
+	
+}
